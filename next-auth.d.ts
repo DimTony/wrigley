2cdeb7a00e364ee686897205ec49557a307ff580
Dimojiaku Obinna Anthony <dimojiakuo@accessbankplc.com>
2025-06-03 20:08:53 +0100

    profile 90%

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -10,6 +10,7 @@ declare module "next-auth" {
       email?: string;
       role: string;
       username: string;
+      fullName?: string;
       profilePic: string;
     } & DefaultSession;
   }
@@ -19,6 +20,7 @@ declare module "next-auth" {
     image: string;
     role: string;
     username: string;
+    fullName?: string;
     profilePic: string;
   }
 }
